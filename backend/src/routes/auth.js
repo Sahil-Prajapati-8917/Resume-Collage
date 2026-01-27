e7a372d5d98bbecd6232acb62bf1ac62790d2a56
Sahil-Prajapati-8917 <sahilprajapati8917@gmail.com>
2026-01-27 08:57:10 +0530

    chanfe

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { body, validationResult } = require('express-validator');
-const { auth } = require('../middleware/auth');
+const { authenticateToken: auth } = require('../middleware/auth');
 const authController = require('../controllers/authController');
 
 const router = express.Router();
